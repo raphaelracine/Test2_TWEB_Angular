48787e340c29825a94d39d49d7753c50fd63f56b
raphaelracine <raphael.racine@heig-vd.ch>
2016-01-08 17:49:05 +0100

    ...

--- README.md
+++ README.md
@@ -11,12 +11,16 @@ This repository is used for TWEB Test 2 at HEIG-VD school.
 ## Choosen Topic
 The topic I have chosen is one where we have to use the API GitHub.
 
-## Studies API CrunchBase
+## Studies API GitHub
 [Here](https://developer.github.com/v3/) we can find the documentation about the API version 3.
 
-jeton à donner à l'application
+<font color=red>Parler du jeton</font>
+
+### Using GitHub API with npm
+You can find documentation to use GitHub API with the following npm module : [github-api](https://github.com/michael/github)
 
 ## Choice of functionality
+
 
 ## Deploying on Heroku
 I have published my app to heroku (click [here](https://test2racine.herokuapp.com) to see it).
--- public/js/my_angular.js
+++ public/js/my_angular.js
@@ -1,8 +1,24 @@
 (function() {
 	
-	var module = angular.module('Test2Module', []);
+	var app = angular.module('Test2Module', ['ui.router']);
 	
-	module.factory('GitHub', function() {
+	app.config(function($stateProvider, $urlRouterProvider) {
+		
+		$urlRouterProvider.otherwise('/enterUser');
+		
+		$stateProvider.state('enterUser', {
+			url: '/enterUser',
+			templateUrl: '/partials/enterUser.html'
+		})
+		.state('userRepositories', {
+			url: '/userRepositories/:user',
+			templateUrl: '/partials/userRepositories.html',
+			controller: 'UserRepositoriesController'
+		});
+		
+	});
+	
+	app.factory('GitHub', function() {
 		
 		var github = new Github({
   			token: "788c7eb1389a47079296d8faacbe08250c3b7914",
@@ -10,6 +26,23 @@
 		});
 		
 		return github;
+		
+	});
+	
+	app.controller('UserRepositoriesController', function($scope, $stateParams, GitHub, $state) {
+		
+		GitHub.getUser().show($stateParams.user, function(err, user) {
+			
+			if(err) {
+				alert("This user can't be retrieved");
+				$state.go('enterUser');
+			}
+			else {
+				$scope.user = user;
+				console.log($scope.user);
+			}
+			
+		});
 		
 	});
 	
